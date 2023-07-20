e19a4bbbeb2fcd0f718f788197887d4518d403af
Matthew Rosenthal <60559647+Marosenthal18@users.noreply.github.com>
2023-07-19 21:17:05 -0400

    main_altered.py
    Fixed env variable loading in
    added a loop to append video ids into a list to use for the api call

--- whatsupyoutube/main_altered.py
+++ whatsupyoutube/main_altered.py
@@ -7,6 +7,7 @@ import pandas as pd
 import requests
 from dotenv import load_dotenv
 
+
 pp = pprint.PrettyPrinter(indent=4)
 
 
@@ -26,51 +27,57 @@ def create_df(search_results) -> pd.DataFrame:
     return pd.concat(df_array)
 
 
+def main() -> None:
+    load_dotenv()
 
 
-api_key = "AIzaSyBT0b-OYNZX6X9m3jmsRHIuLHzg9wVisek"
 
-channel_id = "UCVRobwJfa8nHMfJI3C8Rgdw"
-page_token = ""
 
-base_url = "https://www.googleapis.com/youtube/v3"
-search_url = (
+    api_key = get_env_var("API_Key")
+
+    channel_id = "UCVRobwJfa8nHMfJI3C8Rgdw"
+    page_token = ""
+
+    base_url = "https://www.googleapis.com/youtube/v3"
+    search_url = (
         f"search?key={api_key}"
         f"&channelId={channel_id}"
         f"&part=snippet,id&order=date&maxResults=1000{page_token}"
     )
 
-request_url = f"{base_url}/{search_url}"
+    request_url = f"{base_url}/{search_url}"
 
-search_response = requests.get(request_url).json()
-search_snippet = search_response["items"]
+    search_response = requests.get(request_url).json()
+    search_snippet = search_response["items"]
 
-#trying to create a loop to iterate thru the different video ids
-"""for item in search_snippet:
-    try:
-        kind = item["id"]["kind"]
-        if kind == 'youtube#video':
-            video_id = item['id']['videoId']   
-    except KeyError:
-        print("error")"""
+    search_df = create_df(search_snippet)
 
-
-search_df = create_df(search_snippet)
-
-video_id = search_df["id.videoId"]
-
+    video_ids = []
+  
+    for item in search_snippet:
+        try:
+            kind = item["id"]["kind"]
+            if kind == "youtube#video":
+                    video_ids.append(item["id"]["videoId"])
+        except KeyError:
+             print ("error")
+    
 
 
-video_url = (
-        f"https://youtube.googleapis.com/youtube/v3/videos?id={video_id}"
-        f"&part=statistics&key={api_key}"
-        f"&maxResults=1000&channelId={channel_id}&order=date"
-        )
 
-video_res = requests.get(video_url).json()
+    #video_id = search_df["id.videoId"]
 
-pp.pprint(video_res)
-pd.set_option('display.max_columns', None)
+    video_url = (
+        f"https://youtube.googleapis.com/youtube/v3/videos?id={video_ids}"
+        f"&part=statistics"
+        f"&key={api_key}"
+    )
 
-#print(search_df.head())
-#print(video_id)
+    video_res = requests.get(video_url).json()
+
+    pp.pprint(video_res)
+    #print(video_ids)
+
+
+main()
+
