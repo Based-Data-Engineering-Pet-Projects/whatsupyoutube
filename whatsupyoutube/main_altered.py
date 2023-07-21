b2f41f545d54c30cf3931089dfe26bd0ae19e6d4
Matthew Rosenthal <60559647+Marosenthal18@users.noreply.github.com>
2023-07-20 23:32:14 -0400

    Update main_altered.py

--- whatsupyoutube/main_altered.py
+++ whatsupyoutube/main_altered.py
@@ -32,26 +32,58 @@ def main() -> None:
 
 
 
-
+    #api url components
     api_key = get_env_var("API_Key")
 
-    channel_id = "UCVRobwJfa8nHMfJI3C8Rgdw"
-    page_token = ""
+    channel_id = "UCX6OQ3DkcsbYNE6H8uQQuVA"
+    
 
+    #constructing the search api url
     base_url = "https://www.googleapis.com/youtube/v3"
     search_url = (
         f"search?key={api_key}"
         f"&channelId={channel_id}"
-        f"&part=snippet,id&order=date&maxResults=1000{page_token}"
+        f"&part=snippet,id&order=date&maxResults=50"
+        
     )
+
+
 
     request_url = f"{base_url}/{search_url}"
 
     search_response = requests.get(request_url).json()
-    search_snippet = search_response["items"]
+    
+    next_page_token = search_response['nextPageToken']
 
+    next_search_url = (
+        f"search?key={api_key}"
+        f"&channelId={channel_id}"
+        f"&part=snippet,id&order=date&maxResults=50"
+        f"&pageToken={next_page_token}"
+        
+    )
+
+    next_search_response = f"{base_url}/{next_search_url}"
+    next_page_search = requests.get(next_search_response)
+
+    #while loop to page all the way through
+    """while next_page_token is not None:
+        next_page_search = next_search_url"""
+         
+
+    search_snippet = search_response["items"]
+    
+    #creating search api dataframe
     search_df = create_df(search_snippet)
 
+
+    
+
+
+    #search dataframe transformations
+
+    
+    #creating statistics dataframe
     video_ids = []
   
     for item in search_snippet:
@@ -62,22 +94,24 @@ def main() -> None:
         except KeyError:
              print ("error")
     
+    video_ids_updated = ','.join(video_ids)
 
 
-
-    #video_id = search_df["id.videoId"]
-
+    #constructing videos url
     video_url = (
-        f"https://youtube.googleapis.com/youtube/v3/videos?id={video_ids}"
+        f"https://youtube.googleapis.com/youtube/v3/videos?id={video_ids_updated}"
         f"&part=statistics"
         f"&key={api_key}"
     )
 
     video_res = requests.get(video_url).json()
+    video_res_items = video_res["items"]
+    #print(create_df(video_res_items))
+    #print(search_df)
+    print(next_page_token)
+   
 
-    pp.pprint(video_res)
-    #print(video_ids)
+    #video statistics dataframe transformations
 
 
 main()
-
